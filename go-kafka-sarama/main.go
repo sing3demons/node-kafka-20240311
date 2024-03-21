93a165866de1a1ad2a45872ee79667bf1e6be800
sing3demons <sing3demons@live.com>
2024-03-21 17:33:36 +0700

    Add graceful shutdown and cleanup tasks

--- go-kafka-sarama/main.go
+++ go-kafka-sarama/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/sing3demons/service-todo/handler"
 )
@@ -18,5 +24,20 @@ func main() {
 	api.Get("/todo", todo.GetTodoList)
 	api.Get("/todo/:id", todo.GetTodo)
 
-	app.Listen(":8080")
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	_ = <-c // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+	fmt.Println("Fiber was successful shutdown.")
+
 }
